b90dda46ada758ce276b5e62a4754fe440b19cc3
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-12 10:33:03 +0000

    //commit

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,7 +1,10 @@
 import React from "react"
 
 const ImageList=(props)=>{
-  console.log(props.image)
-  return <div>Image List: </div>
+  const image = props.image.map(({description ,id,urls})=>{
+    return <img key={id} src={urls.regular} alt={description}/>
+
+  })
+  return <div>Image List:{image}</div>
 }
 export default ImageList
